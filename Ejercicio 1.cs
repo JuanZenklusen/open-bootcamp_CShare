d9b70ebb570f2b2368570dcc72b0dacc0886c69f
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2022-09-22 16:56:46 -0300

    Create Ejercicio 1.cs

---
+++ Ejercicio 1.cs
@@ -0,0 +1,7 @@
+/*Ejercicio 1
+
+Escribe un programa que reciba tu nombre y lo escriba por consola.*/
+
+Console.WriteLine("Por favor, introduzca su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Su nombre es " + nombre);
