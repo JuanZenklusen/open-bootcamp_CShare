265af2fc019ed077362c578b033e2635f2967810
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2022-09-22 18:53:11 -0300

    Delete Ejercicio_2.cs

--- Ejercicio_2.cs
+++
@@ -1,5 +0,0 @@
-/*Ejercicio 2
-Escribe un programa que tome la hora y la escriba por pantalla.*/
-
-string horaActual = DateTime.Now.ToString("hh:mm:ss tt");
-Console.WriteLine("La hora actual es: " + horaActual);
